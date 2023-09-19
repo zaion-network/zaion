8b84739097fdb14c13fa581acd0d61f85f132a03
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-09-19 17:46:43 +0200

    adds callback, create mostraCategorie

--- apps/spesa/src/index.ts
+++ apps/spesa/src/index.ts
@@ -23,12 +23,12 @@ const title = document.createElement("h1")
 
 const map = new Map()
 
-const createInput = (id: string) => {
+const createInput = (id: string, cb: (value: string) => void) => {
   const input = document.createElement("input")
   input.addEventListener("keydown", (e) => {
     const condition = e.key === "Enter"
     if (condition) {
-      console.log(input.value);
+      cb(input.value)
       input.value = ""
     }
   })
@@ -43,16 +43,31 @@ body?.appendChild(title)
 
 const productFields = () => {
   const div = document.createElement("div")
-  div.appendChild(createInput("link"))
-  div.appendChild(createInput("name"))
+  div.appendChild(createInput("link", (e) => console.log(e)
+  ))
+  div.appendChild(createInput("name", (e) => console.log(e)))
   body?.appendChild(div)
 }
 
 const categoriaFields = () => {
   const div = document.createElement("div")
-  div.appendChild(createInput("nome"))
+  div.appendChild(createInput("nome", (e) => {
+    console.log(e)
+    new Categoria({ nome: e })
+  }))
+  body?.appendChild(div)
+}
+
+const mostraCategorie = (categories: Categoria[]) => {
+  const div = document.createElement("div")
+  categories.forEach(c => {
+    const p = document.createElement("p")
+    p.textContent = c.value.nome
+    div.appendChild(p)
+  })
   body?.appendChild(div)
 }
 
 productFields()
 categoriaFields()
+mostraCategorie(Categoria.categories)
