fd52767f5fd75ac961a2c7b51387135246dd102d
Giacomo Gagliano <giacomo.gagliano@me.com>
2023-09-23 16:23:44 +0000

    crea index.ts

---
+++ packages/server/src/index.ts
@@ -0,0 +1,5 @@
+import { getFileString } from "./utils/getFileString";
+import { sendFileResponse } from "./utils/sendFileResponse";
+export { SimpleServer } from "./SimpleServer";
+export { getFileString, sendFileResponse };
+export { creaVDOM } from "./utils/createVDOM";
